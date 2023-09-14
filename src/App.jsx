539558f98796b406eaaf1867baa3163203173ea0
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-09-15 02:08:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-3xl font-bold text-center m-12'>
+    <h1>Course Registration</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
