a6568f5010849a97774bb4e223296ac64610be07
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-09-15 05:29:08 +0600

    cart section added

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,7 +2,12 @@
 const Cart = () => {
     return (
         <div>
-           
+            <h1 className="text-2xl font-bold mb-5 text-blue-600">Credit Hour Remaining: 20 hr</h1>
+            <h1 className="text-2xl font-bold ">Course Name</h1>
+            <br />
+            <h3 className="text-lg font-semibold ">Total Credit Hour : 00 hr</h3>
+            <br />
+            <h3 className="text-lg font-semibold ">Total Price : 48000 USD</h3>
         </div>
     );
 };
